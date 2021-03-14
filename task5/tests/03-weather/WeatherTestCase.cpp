c21c45ecc34622fdf475f8b1b14314062a61b7b9
juliangelok <carnatriya@gmail.com>
2021-03-14 04:05:28 +0300

    gmock

--- task5/tests/03-weather/WeatherTestCase.cpp
+++ task5/tests/03-weather/WeatherTestCase.cpp
@@ -7,14 +7,41 @@
 
 
 TEST_F(WeatherTestCase, testGetDifferenceStringGood) {
-    WeatherMockGood w;
+    cpr::Response ten;
+    ten.status_code = 200;
+    ten.text = "{\"list\":[{\"main\":{\"temp\": 10.0}}]}";
+
+    cpr::Response twenty;
+    twenty.status_code = 200;
+    twenty.text = "{\"list\":[{\"main\":{\"temp\": 20.0}}]}";
+
+    
+    WeatherMock w;
     w.SetApiKey("");
 
+    EXPECT_CALL(w, Get)
+        .Times(2)
+        .WillOnce(testing::Return(ten))
+        .WillOnce(testing::Return(twenty));
+
     ASSERT_NO_THROW(w.GetDifferenceString("warsaw", "london"));
+    
+    EXPECT_CALL(w, Get)
+        .Times(2)
+        .WillOnce(testing::Return(twenty))
+        .WillOnce(testing::Return(ten));
+
     ASSERT_NO_THROW(w.GetDifferenceString("london", "warsaw"));
 }
 
 TEST_F(WeatherTestCase, testGetGetDifferenceStringbad) {
-    WeatherMockBad w;
+    cpr::Response bad;
+    bad.status_code = 400;
+    
+    WeatherMock w;
+    EXPECT_CALL(w, Get)
+        .Times(1)
+        .WillOnce(testing::Return(bad));
+
     ASSERT_THROW(w.GetDifferenceString("asdf", "fdsa"), std::invalid_argument);
 }
